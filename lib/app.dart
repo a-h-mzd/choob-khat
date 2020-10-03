8375a3382fe34bc4f6a7bc647ce16844621d4487
amir hosein mzd <amirmohammadi1994@gmail.com>
2020-10-03 16:53:31 +0330

    added biometrics

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:choob_khat/pages/main.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -12,7 +13,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => Scaffold(),
+        '/': (context) => MainPage(),
       },
     );
   }
